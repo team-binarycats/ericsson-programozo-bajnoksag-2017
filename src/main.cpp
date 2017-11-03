aa19e930e5467b48fa87c5b2927e717b91fde189
Domonkos Lezsák <lezsakdomi1@gmail.com>
2017-11-03 11:15:46 +0100

    Send login message

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,39 @@
  * Currently it just connects to a given server using a given username and hash
  */
 
+#include "Request.capnp.h"
+#include "Response.capnp.h"
+//#include "Bugfix.capnp.h"
+
+#include <capnp/message.h>
+#include <capnp/serialize-packed.h>
+
 #include <iostream>
 
 using namespace ericsson2017::protocol::test;
 
-char* server;
 char* hash;
 char* username;
 int main(int argc, char* argv[]) {
 	if (argc != 4) {
-		std::cerr<<"Usage: "<<argv[0]<<" server username hash"<<std::endl;
+		std::cerr<<"Usage: "<<argv[0]<<" username hash"<<std::endl;
 		return 1;
 	} else {
-		server = argv[1];
-		std::cerr<<"Using server "<<server<<std::endl;
-
 		username = argv[2];
 		std::cerr<<"Using username "<<username<<std::endl;
 
 		hash = argv[3];
 		std::cerr<<"Using hash "<<hash<<std::endl;
+	}
+
+	{
+		::capnp::MallocMessageBuilder message;
+		Request::Builder request = message.initRoot<Request>();
+		Request::Login::Reader login = message.getLogin();
+		login.setTeam(username);
+		login.setHash(hash);
+		
+		::capnp::writePackedMessageToFd(1, message);
 	}
 
 	return 0;
