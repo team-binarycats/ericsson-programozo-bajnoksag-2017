65f7bbf823d04ba3e5a60924a5e1f12b95b4f77d
Domonkos Lezsák <lezsakdomi1@gmail.com>
2017-11-11 23:33:21 +0100

    Add bouncing to keyboard

--- src/keyboard.cpp
+++ src/keyboard.cpp
@@ -41,6 +41,19 @@ _MAIN_LOOP {
 			break;
 
 		default:
-			move.setDirection(response.getUnits()[0].getDirection());
+			auto dir = response.getUnits()[0].getDirection();
+			auto x = response.getUnits()[0].getPosition().getX();
+			auto y = response.getUnits()[0].getPosition().getY();
+			if ( dir == Direction::UP && x == 0 ) {
+				move.setDirection(Direction::DOWN);
+			} else if ( dir == Direction::DOWN && x == 79 ) {
+				move.setDirection(Direction::UP);
+			} else if ( dir == Direction::LEFT && y == 0 ) {
+				move.setDirection(Direction::RIGHT);
+			} else if ( dir == Direction::RIGHT && y == 99 ) {
+				move.setDirection(Direction::LEFT);
+			} else {
+				move.setDirection(response.getUnits()[0].getDirection());
+			}
 	}
 }
