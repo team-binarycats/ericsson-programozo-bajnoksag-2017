22eec65a28b5843e717769cdd70ec521e58eb64f
Domonkos Lezsák <lezsakdomi1@gmail.com>
2017-11-12 09:00:35 +0100

    Basic collision detection

--- src/sequential.cpp
+++ src/sequential.cpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <cstdlib>
 #include <ncurses.h>
+#include <functional>
+#include <vector>
 
 using namespace ericsson2017::protocol;
 
@@ -17,7 +19,10 @@ enum Stage {
 	cont,	// Continue it
 	end,	// Go back to my area
 	prep,	// Prepare for next square: Move to starting position
-	turn	// Make a U-turn
+	turn,	// Make a U-turn
+	stale_begin,
+	stale_cont,
+	stale_end,
 } stage;
 struct SquareStatus {
 	int32_t x, y;
@@ -61,7 +66,67 @@ _SETUP {
 	}
 }
 
+struct E {
+	int x, y;
+	short signed xd, yd;
+	E(const Enemy::Reader& enemy){
+		x = enemy.getPosition().getX();
+		y = enemy.getPosition().getY();
+		switch (enemy.getDirection().getVertical()) {
+			case Direction::UP:
+				xd = -1;
+				break;
+
+			case Direction::DOWN:
+				xd = 1;
+				break;
+
+		}
+		switch (enemy.getDirection().getHorizontal()) {
+			case Direction::LEFT:
+				yd = -1;
+				break;
+
+			case Direction::RIGHT:
+				yd = 1;
+				break;
+		}
+	}
+};
+
 _MAIN_LOOP {
+	auto ismy = [&response](int x, int y){
+		return response.getCells()[x][y].getOwner()==1;
+	};
+
+	std::function<bool(size_t, size_t, size_t, std::vector<E>)> recfreetil = [&](size_t x, size_t y, size_t time, std::vector<E> es){
+		for (int i=0; i<es.size(); i++) {
+			if ( es[i].x==x && es[i].y==y ) return false;
+			es[i].x+=es[i].xd;
+			es[i].y+=es[i].yd;
+			if (ismy(es[i].x, es[i].y)) {
+				E e2 = es[i];
+				es[i].xd=-es[i].xd; es[i].x+=2*es[i].xd;
+				es[i].yd=-es[i].yd; es[i].y+=2*es[i].yd;
+				if (!ismy(e2.x-2*e2.xd, e2.y)) {
+					e2.xd=-e2.xd; e2.x+=2*e2.xd;
+					es.push_back(e2);
+				} else if (!ismy(e2.x, e2.y-2*e2.yd)) {
+					e2.yd=-e2.yd; e2.y+=2*e2.yd;
+					es.push_back(e2);
+				}
+			}
+		}
+		if (time==0) return true;
+		else return recfreetil(x, y,time-1, es);
+	};
+
+	auto freetil = [&](size_t x, size_t y, size_t time){
+		std::vector<E> es;
+		for (auto enemy : response.getEnemies()) es.push_back(E(enemy));
+		return recfreetil(x, y,time, es);
+	};
+
 	switch (getch()) {
 		case KEY_HOME:
 			exit(0);
@@ -102,7 +167,19 @@ _MAIN_LOOP {
 			}
 			break;
 
+		case stale_begin:
+			move.setDirection(Direction::RIGHT);
+			stage = begin;
+			break;
+
 		case begin:
+			if (!freetil(response.getUnits()[0].getPosition().getX(), response.getUnits()[0].getPosition().getY()+1, a-status.cnt)) {
+				log("Possible collision detected. Stalling for safety.");
+				move.setDirection(Direction::LEFT);
+				stage = stale_begin;
+				break;
+			}
+
 			if ( !status.saved ) {
 				status.x = response.getUnits()[0].getPosition().getX();
 				status.y = response.getUnits()[0].getPosition().getY();
@@ -116,7 +193,19 @@ _MAIN_LOOP {
 			}
 			break;
 
+		case stale_cont:
+			move.setDirection(status.direction);
+			stage = cont;
+			break;
+
 		case cont:
+			if (!freetil(response.getUnits()[0].getPosition().getX()+(status.direction==Direction::DOWN?1:-1), response.getUnits()[0].getPosition().getY(), 2*a-status.cnt)) {
+				log("Possible collision detected. Stalling for safety.");
+				move.setDirection(opposite(status.direction));
+				stage = stale_cont;
+				break;
+			}
+
 			move.setDirection(status.direction);
 			if ( ++status.cnt == a ) {
 				status.cnt = 0;
@@ -132,7 +221,19 @@ _MAIN_LOOP {
 			}
 			break;
 
+		case stale_end:
+			move.setDirection(Direction::LEFT);
+			stage = end;
+			break;
+
 		case end:
+			if (!freetil(response.getUnits()[0].getPosition().getX(), response.getUnits()[0].getPosition().getY()-1, a-status.cnt)) {
+				log("Possible collision detected. Stalling for safety.");
+				move.setDirection(Direction::RIGHT);
+				stage = stale_end;
+				break;
+			}
+
 			move.setDirection(Direction::LEFT);
 			if ( ++status.cnt == a ) {
 				log("Square finished");
