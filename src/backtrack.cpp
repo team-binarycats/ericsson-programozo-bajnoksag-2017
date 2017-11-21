e1a047046f9a4ea57efacdfdc81412497045ae37
Domonkos Lezsák <lezsakdomi1@gmail.com>
2017-11-21 20:30:20 +0100

    Add pos::operator string

--- src/backtrack.cpp
+++ src/backtrack.cpp
@@ -111,6 +111,10 @@ struct Pos {
 	Pos(X x, Y y) : x(x), y(y) {}
 	Pos(const ericsson2017::protocol::Position::Reader pos) : Pos(pos.getX(), pos.getY()) {}
 
+	operator string() const {
+		return (string)"("+to_string(x)+(string)","+to_string(y)+(string)")";
+	}
+
 	template <typename T>
 	operator T() const {
 		return x * X::MAX + y;
