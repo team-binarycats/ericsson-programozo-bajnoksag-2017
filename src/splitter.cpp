471765641d7442cbb707bd68b3aa1af94ab4b4f8
Domonkos Lezsák <lezsakdomi1@gmail.com>
2017-11-22 21:15:42 +0100

    Implement the doit stage

--- src/splitter.cpp
+++ src/splitter.cpp
@@ -5,7 +5,7 @@ using namespace std;
 using namespace ericsson2017::protocol;
 
 const unsigned split = 3;
-int current_col; // negative: vertical, positive: horizontal
+unsigned current_col; // even: horizontal, odd: vertical
 unsigned checkpoint; // last safe x (or y) in current col
 unsigned cnt;
 enum {
@@ -39,6 +39,14 @@ _MAIN_LOOP {
 			break;
 
 		case doit:
+			if (safe(response,
+						response.getUnits()[0].getPosition().getX()+(  (current_col%2) ? +1 : 0 ),
+						response.getUnits()[0].getPosition().getY()+( !(current_col%2) ? +1 : 0 )
+			)) {
+				move.setDirection(current_col%2 ? Direction::DOWN : Direction::RIGHT);
+			} else {
+				move.setDirection(current_col%2 ? Direction::UP : Direction::LEFT);
+			}
 			break;
 	}
 }
