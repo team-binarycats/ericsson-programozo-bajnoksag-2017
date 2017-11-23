e66e8f4da042d0205670a69f1015ece61ea571fb
Domonkos Lezsák <lezsakdomi1@gmail.com>
2017-11-23 18:23:07 +0100

    Implement move_to && make the code working (aka fixes)

--- src/splitter.cpp
+++ src/splitter.cpp
@@ -1,8 +1,8 @@
 #include "main.h"
 #include "interface.h"
 #include <bits/stdc++.h>
-using namespace std;
 using namespace ericsson2017::protocol;
+using namespace std;
 
 Direction opposite (Direction direction) {
 	switch (direction) {
@@ -40,6 +40,22 @@ Direction prev (Direction direction) {
 	return next(next(next(direction)));
 }
 
+string to_string(Direction direction) {
+	switch (direction) {
+		case Direction::UP:
+			return "UP";
+
+		case Direction::LEFT:
+			return "LEFT";
+
+		case Direction::DOWN:
+			return "DOWN";
+
+		case Direction::RIGHT:
+			return "RIGHT";
+	}
+}
+
 enum {
 	move_to,
 	doit,
@@ -65,15 +81,17 @@ void reset_split(){
 	split=3;
 }
 void next_split(){
+	log("Next split");
 	split*=2;
 	// no maximum here :)
 }
 
 bool vertical;
 void reset_vertical(){
-	vertical=false;
+	vertical=true;
 }
 void next_side(){
+	log("Next side");
 	if (vertical==true) {
 		reset_vertical();
 		next_split();
@@ -87,9 +105,10 @@ void next_side(){
 
 unsigned col;
 void reset_col(){
-	col=0;
+	col=1;
 }
 void next_col(){
+	log("Next column");
 	if (++col==split) {
 		reset_col();
 		next_side();
@@ -112,10 +131,13 @@ _SETUP {
 		case SetupReason::INIT:
 		case SetupReason::LEVELUP:
 		case SetupReason::LEVELDOWN:
+			reset_direction();
+			reset_side_direction();
 			reset_split();
 			reset_checkpoint();
 			reset_vertical();
 			reset_col();
+			log("Reset completed");
 			break;
 
 		case SetupReason::DEATH:
@@ -132,9 +154,55 @@ bool safe(const Response::Reader&, unsigned, size_t, size_t) {
 }
 
 _MAIN_LOOP {
+	if ( stage == doit ) {
+		switch (direction) {
+			case Direction::DOWN:	if (response.getUnits()[0].getPosition().getX()>=78)	next_col(); break;
+			case Direction::UP:	if (response.getUnits()[0].getPosition().getX()<=1)	next_col(); break;
+			case Direction::RIGHT:	if (response.getUnits()[0].getPosition().getY()>=98)	next_col(); break;
+			case Direction::LEFT:	if (response.getUnits()[0].getPosition().getY()<=1)	next_col(); break;
+		}
+	}
+
 	switch (stage) {
 		case move_to:
-			//TODO code here
+			{
+				size_t target_x, target_y;
+				if (vertical) {
+					target_y = (size_t)100/split*col-1;
+				} else {
+					target_x = (size_t)80/split*col-1;
+				}
+
+				switch (direction) {
+					case Direction::DOWN:	target_x = 1 + checkpoint;	break;
+					case Direction::RIGHT:	target_y = 1 + checkpoint;	break;
+					case Direction::UP:	target_x = 78 - checkpoint;	break;
+					case Direction::LEFT:	target_y = 98 - checkpoint;	break;
+				}
+
+				signed v_diff = target_x - response.getUnits()[0].getPosition().getX();
+				signed h_diff = target_y - response.getUnits()[0].getPosition().getY();
+
+				log((string)"Target: ("+to_string(target_x)+(string)","+to_string(target_y)+(string)") "+to_string(split)+(string)" "+to_string(col)+" "+(string)(vertical?"v":"h")+(string)" "+to_string(direction));
+
+				if ( abs(v_diff) > abs(h_diff) ) {
+					if ( v_diff > 0 ) {
+						move.setDirection(Direction::DOWN);
+					} else { // v_diff < 0
+						move.setDirection(Direction::UP);
+					}
+				} else { // h_diff > v_diff
+					if ( h_diff > 0 ) {
+						move.setDirection(Direction::RIGHT);
+					} else { // h_diff < 0
+						move.setDirection(Direction::LEFT);
+					}
+				}
+
+				if ( abs(v_diff)+abs(h_diff) == 1 ) {
+					stage = doit;
+				}
+			}
 			break;
 
 		case doit:
@@ -147,16 +215,6 @@ _MAIN_LOOP {
 			} else {
 				move.setDirection(opposite(direction));
 				cnt--;
-			}
-
-			if (vertical) {
-				if (response.getUnits()[0].getPosition().getX()>=78) {
-					next_col();
-				}
-			} else { // horizontal
-				if (response.getUnits()[0].getPosition().getY()>=98) {
-					next_col();
-				}
 			}
 			break;
 	}
