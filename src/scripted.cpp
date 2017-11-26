265f8f36d6119d7dccfa78f686639027575fd733
Domonkos Lezsák <lezsakdomi1@gmail.com>
2017-11-26 12:19:57 +0100

    Add scripted program

---
+++ src/scripted.cpp
@@ -0,0 +1,46 @@
+#include "main.h"
+
+#include "interface.h"
+#include <cstdlib>
+#include <cstring>
+#include <stdio.h>
+
+using namespace ericsson2017::protocol;
+
+void trim(char * s) {
+	char * p = s;
+	int l = strlen(p);
+	
+	while(isspace(p[l - 1])) p[--l] = 0;
+	while(* p && isspace(* p)) ++p, --l;
+	
+	memmove(s, p, l + 1);
+}
+
+_SETUP {}
+
+FILE* cs = fdopen(5, "r");
+char* cmd = NULL;
+size_t len = 0;
+ssize_t read;
+
+_MAIN_LOOP {
+	move.setUnit(0);
+	
+	while (true) {
+		if (getline(&cmd, &len, cs)<2) throw std::length_error("Unable to read next command");
+		trim(cmd);
+
+		if ( cmd[0]==']' ) {
+			log((std::string)"Comment: "+std::string(cmd));
+			continue;
+		}
+		else if ( strcmp(cmd, "LEFT"	) == 0 ) move.setDirection(Direction::LEFT	);
+		else if ( strcmp(cmd, "RIGHT"	) == 0 ) move.setDirection(Direction::RIGHT	);
+		else if ( strcmp(cmd, "UP"	) == 0 ) move.setDirection(Direction::UP	);
+		else if ( strcmp(cmd, "DOWN"	) == 0 ) move.setDirection(Direction::DOWN	);
+		else throw std::domain_error((std::string)"Could not understand command \""+(std::string)cmd+(std::string)"\"");
+
+		break;
+	}
+}
